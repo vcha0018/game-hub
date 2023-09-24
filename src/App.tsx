04efc331f554f66026511b70b976a3a8d3f6b4da
vcha0018 <vkc9191@gmail.com>
2023-09-24 19:49:42 +1000

    Build Color Mode Switch.

--- src/App.tsx
+++ src/App.tsx
@@ -9,17 +9,13 @@ function App() {
         base: `"nav" "main"`,
         lg: `"nav nav" "aside main"`,
       }}>
-      <GridItem area={"nav"}>
+      <GridItem area="nav">
         <NavBar />
       </GridItem>
       <Show above="lg">
-        <GridItem area={"aside"} bg="gold">
-          Aside
-        </GridItem>
+        <GridItem area="aside">Aside</GridItem>
       </Show>
-      <GridItem area={"main"} bg="dodgerblue">
-        Main
-      </GridItem>
+      <GridItem area="main">Main</GridItem>
     </Grid>
   );
 }
