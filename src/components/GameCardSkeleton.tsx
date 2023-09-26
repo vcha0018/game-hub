829dbb8a89c869f8b1613ff90efa985e76406bf9
vcha0018 <vkc9191@gmail.com>
2023-09-26 15:52:12 +1000

    Show Loading Skeletons

---
+++ src/components/GameCardSkeleton.tsx
@@ -0,0 +1,29 @@
+import {
+  Card,
+  CardBody,
+  HStack,
+  Skeleton,
+  SkeletonText,
+} from "@chakra-ui/react";
+
+const GameCardSkeleton = () => {
+  return (
+    <Card width="300px" borderRadius={10} overflow={"hidden"}>
+      <Skeleton height="200px" />
+      <CardBody>
+        <SkeletonText noOfLines={2} skeletonHeight={2} />
+        <HStack marginY={4} justifyContent="space-between">
+          <Skeleton height="15px" width="10rem" />
+          <Skeleton
+            height="15px"
+            width="30px"
+            startColor="grey.600"
+            endColor="green.600"
+          />
+        </HStack>
+      </CardBody>
+    </Card>
+  );
+};
+
+export default GameCardSkeleton;
