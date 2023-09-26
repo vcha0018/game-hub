af6c069e36b8490898659f37a43b40859c02d37a
vcha0018 <vkc9191@gmail.com>
2023-09-26 17:10:51 +1000

    Loading Skeleton for Genres.

--- src/components/GenreList.tsx
+++ src/components/GenreList.tsx
@@ -1,13 +1,18 @@
 import { HStack, Image, List, ListItem, Text } from "@chakra-ui/react";
 import useGenres from "../hooks/useGenres";
 import getCroppedImageUrl from "../services/image-url";
+import GenreSkeleton from "./GenreSkeleton";
 
 const GenreList = () => {
-  const { data } = useGenres();
+  const { data, isLoading, error } = useGenres();
+  const skeletons = [1, 2, 3, 4, 5, 6, 7, 8, 9];
 
+  if (error) return null;
   return (
     <>
       <List>
+        {isLoading &&
+          skeletons.map((skeletion) => <GenreSkeleton key={skeletion} />)}
         {data.map((genre) => (
           <ListItem key={genre.id} marginY={2}>
             <HStack>
---
+++ src/components/GenreSkeleton.tsx
@@ -0,0 +1,14 @@
+import { HStack, Skeleton, VStack } from "@chakra-ui/react";
+
+const GenreSkeleton = () => {
+  return (
+    <VStack marginY={2} align="stretch">
+      <HStack justifyContent="flex-start">
+        <Skeleton height="32px" width="32px" />
+        <Skeleton height="10px" width="6rem" />
+      </HStack>
+    </VStack>
+  );
+};
+
+export default GenreSkeleton;
