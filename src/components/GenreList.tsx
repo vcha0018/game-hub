c45b9393cf0a601231b5dbd4dc179462d2a82fce
vcha0018 <vkc9191@gmail.com>
2023-09-27 15:34:55 +1000

    Highlight selected Genre.

--- src/components/GenreList.tsx
+++ src/components/GenreList.tsx
@@ -5,9 +5,10 @@ import GenreSkeleton from "./GenreSkeleton";
 
 interface Props {
   onSelectGenre: (genre: Genre) => void;
+  selectedGenre: Genre | null;
 }
 
-const GenreList = ({ onSelectGenre }: Props) => {
+const GenreList = ({ selectedGenre, onSelectGenre }: Props) => {
   const { data, isLoading, error } = useGenres();
   const skeletons = [1, 2, 3, 4, 5, 6, 7, 8, 9];
 
@@ -26,6 +27,7 @@ const GenreList = ({ onSelectGenre }: Props) => {
                 borderRadius={2}
               />
               <Button
+                fontWeight={genre.id === selectedGenre?.id ? "bold" : "normal"}
                 variant="link"
                 fontSize="lg"
                 onClick={() => onSelectGenre(genre)}>
