081fe51a316b54b5e05b7b869f26ca8dc8b015c0
vcha0018 <vkc9191@gmail.com>
2023-10-27 19:53:41 +1100

    Build game trailer.

---
+++ src/hooks/useTrailer.ts
@@ -0,0 +1,14 @@
+import { useQuery } from "@tanstack/react-query";
+import { Trailer } from "../entities/Trailer";
+import APIClient from "../services/apiClient";
+
+const useTrailer = (gameID: number) => {
+  const apiClient = new APIClient<Trailer>(`games/${gameID}/movies`);
+
+  return useQuery({
+    queryKey: ["trailers", gameID],
+    queryFn: apiClient.getAll,
+  });
+};
+
+export default useTrailer;
